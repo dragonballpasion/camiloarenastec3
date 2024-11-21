5f35fabc59e6f008cb342cb1ad9a273c20a8c4b3
camilloooyymarto <camiloarenas2712@gmail.com>
2024-11-21 19:14:31 -0300

    Primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+public class TablaMultiplicar
+{
+    public static void Main(string[] args)
+    {
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("{0}x{1}= {2}", x, i, x * i);
+
+        }
+        Console.ReadKey();
+    }
+
+}
